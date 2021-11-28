8424d73266244a7335dae0cbfa6caeedb3222b0a
Wrufesh <wrufesh@gmail.com>
2021-11-28 17:50:41 +0545

    password visibility functionality added and input fields made reactive

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,12 +1,45 @@
-import { Component } from 'react';
+import { Component, ReactElement } from 'react';
 
 import {ReactComponent as Eye} from '@/icons/eye.svg'
 import {ReactComponent as EyeOff} from '@/icons/eye-off.svg'
 
-class Login extends Component {
-    constructor(){
-        this.state = {
-            showPassword: false
+type MyProps = {};
+type MyState = {
+    showPassword: Boolean;
+    email: string;
+    password: string;
+};
+
+class Login extends Component<MyProps, MyState> {
+    state: MyState = {
+        showPassword: false,
+        email: '',
+        password: ''
+    }
+
+    toggleShowPassword(): void{
+        this.state.showPassword = !this.state.showPassword
+    }
+
+    onChange = (e: React.FormEvent<HTMLInputElement>, inputName: string): void => {
+        let update: {[key: string]: any} = {}
+        update[inputName] = e.currentTarget.value
+        this.setState(update as Pick<MyState, keyof MyState>);
+    };
+
+    passwordInputType(): string {
+        if (this.state.showPassword){
+            return 'text'
+        } else {
+            return 'password'
+        }
+    }
+
+    getPasswordFieldIcon(): ReactElement{
+        if (this.state.showPassword){
+            return <EyeOff />
+        } else {
+            return <Eye />
         }
     }
 
@@ -14,14 +47,21 @@ class Login extends Component {
         return <div className="w-2/4">
             <div className="flex flex-col">
                 <label>Email</label>
-                <input type="email"/>
+                <input 
+                    type="email" 
+                    value={this.state.email} 
+                    onChange={(e) => this.onChange(e, 'email')} 
+                />
             </div>
             <div className="flex flex-col">
                 <label>Password</label>
-                <div>
-                <input type="password" />
-                <Eye/>
-                <EyeOff/>
+                <div className="flex">
+                    <input 
+                        type={this.passwordInputType()} 
+                        value={this.state.password} 
+                        onChange={(e) => this.onChange(e, 'password')} 
+                    />
+                    {this.getPasswordFieldIcon()}
                 </div>
             </div>
         </div>
